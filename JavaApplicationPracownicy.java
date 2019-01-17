711ae41aeb85faa4c0f06765cc5715a6cc592f04
KVavilon13 <45732538+KVavilon13@users.noreply.github.com>
2019-01-17 19:47:47 +0100

    Create JavaApplicationPracownicy.java
    nie ma

---
+++ JavaApplicationPracownicy.java
@@ -0,0 +1,19 @@
+
+package javaapplicationpracownicy;
+
+
+public class JavaApplicationPracownicy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+     
+        Pracownik pr1 = new Pracownik("faddfa","afadfa",12,"M");
+        Kierowca kr1= new Kierowca("sdf","afadfa",12,"M","dasd","sad","sad");
+        System.out.println(kr1.getName());
+        
+    }
+    
+}
